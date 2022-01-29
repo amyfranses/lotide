8618615ee7bfcaacfa5dfc12b153b5968f4d4359
amyfranses <amyfranses@gmail.com>
2022-01-29 06:57:50 +0000

    created a function to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2){
+  if (arr1.toString() === arr2.toString()){
+    return true;
+  } else {
+    return false;
+  }
+};
