3e6f7227de4d5f4fc051b9182df9ea753560df85
amyfranses <amyfranses@gmail.com>
2022-01-30 02:58:40 +0000

    updated flatten funtion

--- flatten.js
+++ flatten.js
@@ -1,6 +1,12 @@
-const flatten = function (inputArray){
-  let output = inputArray.flat(2);
-  return output;
+const flatten = function(inputArray) {
+  let newArray = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    if (Array.isArray(inputArray[i])) {
+      newArray = newArray.concat(flatten(inputArray[i]));
+    } else {
+      newArray.push(inputArray[i]);
+    }
+  }
+  return newArray;
 };
 
-console.log(flatten([1, 2, [3, 4], 5, [6]]) );
