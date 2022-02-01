bccde53199cc8f79141a75fec2311a005634628a
amyfranses <amyfranses@gmail.com>
2022-02-01 21:15:55 +0000

    added eqObject function to build on for assert Objects Equal function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,26 @@
+function eqArrays(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+const eqObjects = function(object1, object2) {
+  let key1 = Object.keys(object1);
+  let key2 = Object.keys(object2);
+
+  if (key1.length !== key2.length) {
+    return false;
+  }
+  for (let key of key1) {
+    if (Array.isArray(object1[key]) && (Array.isArray(object2[key]))) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
