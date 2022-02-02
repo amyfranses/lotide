bbc6660804b4251784df3071c116265248cdd7ac
amyfranses <amyfranses@gmail.com>
2022-02-02 21:13:14 +0000

    updated eqObjects function with new test cases

--- eqObjects.js
+++ eqObjects.js
@@ -16,33 +16,35 @@ function eqArrays(arr1, arr2) {
 const eqObjects = function(object1, object2) {
   let key1 = Object.keys(object1);
   let key2 = Object.keys(object2);
-
+  // check length 
   if (key1.length !== key2.length) {
     return false;
   }
-  for (let key of key1) {
+  // use for of loop to loop through key1 and find each key
+    for (let key of key1) {  
+      // check keys as an array and compare against each object
     if (Array.isArray(object1[key]) && (Array.isArray(object2[key]))) {
+      // if eqArrays is not equal return false
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
     } else if (object1[key] !== object2[key]) {
+      // if object 1 key and object 2 key arent equal return false
       return false;
     }
   }
+  // otherwise return true
   return true;
 };
 
+// test cases
 // const cd = { c: "1", d: ["2", 3] };
 // const dc = { d: ["2", 3], c: "1" };
-// eqObjects(cd, dc); // => true
+// assertEqual(eqObjects(cd, dc), true);
 
 // const cd2 = { c: "1", d: ["2", 3, 4] };
-// console.log(eqObjects(cd, cd2)); // => false
+// assertEqual(eqObjects(cd, cd2), false); // => false
 
-// const ab = {a: "1", b: "2"};
-// const ba = {b: "2", a: "1"};
-// console.log(eqObjects(ab, ba));
+// const ab = { a: "1", b: ["2", 3] };
 
-// const abc = {a: "1", b: "2", c: "3"};
-// console.log(eqObjects(ab, abc));
-// eqObjects(ab, abc);
+// assertEqual(eqObjects(cd, ab), false);
