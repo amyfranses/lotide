89d0e1eca3db70645221c3c16d0f547570ca2aed
amyfranses <amyfranses@gmail.com>
2022-02-04 01:45:01 +0000

    added map.js to library with 3 test case examples

---
+++ map.js
@@ -0,0 +1,43 @@
+function eqArrays(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function assertArraysEqual(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅Assertion Passed  ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed  ${arr1} !== ${arr2}`);
+  }
+}
+
+const map = function(array, callback) {
+  const results = [];
+  for (const item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+// test code
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+console.log(results1);
+assertArraysEqual(["g", "c", "t", "m", "t"], [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(["g", "c", "t", "m", "t"], ["g", "c", "t", "m", "t"]);
+
+const animals = ["dog", "pig", "cat", "cow"];
+const results2 = map(animals, animal => animal + "s");
+console.log(results2);
+assertArraysEqual(["g", "c", "t", "m", "t"], ["dog", "pig", "cat", "cow"]);
+assertArraysEqual(['dogs', 'pigs', 'cats', 'cows'], ['dogs', 'pigs', 'cats', 'cows']);
+
+const names = ["matt", "jen", "charlie", "kate"];
+const results3 = map(names, name => name.toUpperCase());
+console.log(results3);
+assertArraysEqual(['MATT', 'JEN', 'CHARLIE', 'KATE'], ["matt", "jen", "charlie", "kate"]);
+assertArraysEqual(['MATT', 'JEN', 'CHARLIE', 'KATE'], ['MATT', 'JEN', 'CHARLIE', 'KATE']);
