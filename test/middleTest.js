3dee5ef14daadbd5421f6dbf96adb63c755cd447
amyfranses <amyfranses@gmail.com>
2022-02-15 02:22:20 +0000

    used M & C for assertion tests on middle.js function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,16 @@
+// const middle = require("../middle");
+// const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), 2);
+  });
+  it("returns 3 for [1, 2, 3, 4, 5]", () => {
+    assert.strictEqual(middle([1, 2, 3, 4, 5]), 3);
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
