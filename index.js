68999377b3f197034a15b3ad2b8fe024b80a1a9b
amyfranses <amyfranses@gmail.com>
2022-02-16 03:55:09 +0000

    refactored index.js and cleaned up text

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
+// Equality comparison
+const assertEqual = require("./assertEqual");
+// Array comparison and methods
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 const head = require("./head");
 const tail = require("./tail");
 const middle = require("./middle");
-const assertArraysEqual = require("./assertArraysEqual");
-const assertEqual = require("./assertEqual");
-const assertObjectsEqual = require("./assertObjectsEqual");
-const countLetters = require("./countLetters");
-const countOnly = require("./countOnly");
-const eqArrays = require("./eqArrays");
-const eqObjects = require("./eqObjects");
-const findKey = require("./findKey");
-const findKeyByValue = require("./findKeyByValue");
-const flatten = require("./flatten");
-const letterPositions = require("./letterPositions");
 const map = require("./map");
 const takeUntil = require("./takeUntil");
 const without = require("./without");
+const flatten = require("./flatten");
+// Object comparison and methods
+const eqObjects = require("./eqObjects");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+// String methods
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const letterPositions = require("./letterPositions");
 
 module.exports = {
+  assertEqual,
+  eqArrays,
+  assertArraysEqual,
   head,
   tail,
   middle,
-  assertArraysEqual,
-  assertEqual,
-  assertObjectsEqual,
-  countLetters,
-  countOnly,
-  eqArrays,
-  eqObjects,
-  findKey,
-  findKeyByValue,
-  flatten,
-  letterPositions,
   map,
   takeUntil,
   without,
+  flatten,
+  eqObjects,
+  assertObjectsEqual,
+  findKey,
+  findKeyByValue,
+  countLetters,
+  countOnly,
+  letterPositions,
 };
