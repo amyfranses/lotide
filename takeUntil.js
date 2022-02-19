29a5bdb4c844ec36a0be83f9c2e81bd97d77f864
amyfranses <amyfranses@gmail.com>
2022-02-19 21:41:56 +0000

    remove test code to separate file, add module.exports, clean up comments

--- takeUntil.js
+++ takeUntil.js
@@ -1,47 +1,8 @@
-function eqArrays(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
-function assertArraysEqual(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅Assertion Passed  ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed  ${arr1} !== ${arr2}`);
-  }
-}
-
+// input array and callback, returns a slice of the array based on the callback
 const takeUntil = function (array, callback) {
-  // array.slice() returns new array starting at 0 and ends at
-  // findIndex() using callback
   return array.slice(0, array.findIndex(callback));
 };
-
-// test code
-
-assertArraysEqual([1, 2, 5, 7, 2], [1, 2, 5, 7, 2]);
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results = takeUntil(data1, (x) => x < 0);
-console.log(results);
-
-console.log("---");
-
-assertArraysEqual(["I've", "been", "to", "Hollywood"], [1, 2, 5, 7, 2]);
-const data2 = [
-  "I've",
-  "been",
-  "to",
-  "Hollywood",
-  ",",
-  "I've",
-  "been",
-  "to",
-  "Redwood",
-];
-const results2 = takeUntil(data2, (x) => x === ",");
-console.log(results2);
+module.exports = takeUntil;
