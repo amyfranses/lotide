fb11f1409c171123a027f444d9ae91761284da26
amyfranses <amyfranses@gmail.com>
2022-02-19 20:43:40 +0000

    add module.exports, moved test code to countOnlyTest.js

--- countOnly.js
+++ countOnly.js
@@ -5,13 +5,12 @@ const assertEqual = function (actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
+// given an array of allItems and itemsToCount, return subset of itemsToCount
+// use assertEqual to see if expected result matches actual. 
 const countOnly = function (allItems, itemsToCount) {
-  // empty results object
   const results = {};
   for (const item of allItems) {
     if (itemsToCount[item]) {
-      // if item is in itemsToCount (true) add to results object each time that item appears
       if (results[item]) {
         results[item] += 1;
       } else {
@@ -22,26 +21,6 @@ const countOnly = function (allItems, itemsToCount) {
   return results;
 };
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe",
-];
+module.exports = countOnly;
 
-const result1 = countOnly(firstNames, {
-  Jason: true,
-  Karima: true,
-  Fang: true,
-  Agouhanna: false,
-});
-console.log(result1);
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+
