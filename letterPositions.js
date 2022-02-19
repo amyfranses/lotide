f3d3078b04f4391ad43e1eb9a2519b8da0b1cefc
amyfranses <amyfranses@gmail.com>
2022-02-19 21:21:17 +0000

    remove test code into separate test file, add module.exports

--- letterPositions.js
+++ letterPositions.js
@@ -1,27 +1,11 @@
-function eqArrays(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function assertArraysEqual(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅Assertion Passed  ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed  ${arr1} !== ${arr2}`);
-  }
-}
-
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
+// inputs a string and returns letter positions
 const letterPositions = function (sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
-    // each letter is sentence[i]
     const letter = sentence[i];
     if (letter === " ") {
-      /* if letter = space then move on */
       continue;
     }
     if (results[letter]) {
@@ -33,8 +17,4 @@ const letterPositions = function (sentence) {
   return results;
 };
 
-// test cases:
-const result1 = letterPositions("hello");
-assertArraysEqual(result1["l"], [2]);
-assertArraysEqual(result1["h"], [0]);
-assertArraysEqual(result1["o"], [4]);
+module.exports = letterPositions;
