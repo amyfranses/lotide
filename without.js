5cf9f1e72241b866244ddf38cea9c99a98f9bbcf
amyfranses <amyfranses@gmail.com>
2022-02-19 21:44:26 +0000

    clean up comments, add module.exports

--- without.js
+++ without.js
@@ -1,27 +1,11 @@
-function eqArrays(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
-function assertArraysEqual(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅Assertion Passed  ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed  ${arr1} !== ${arr2}`);
-  }
-}
+// returns source array having removed specified items
 function without(sourceArray, removeItem) {
-  // spread opperator
   let newArray = [...sourceArray];
-  // loop over input array
   for (let i = 0; i < sourceArray.length; i++) {
-    // loop over remove array
     for (let j = 0; j < removeItem.length; j++) {
-      // compare newArray to remove array
       if (newArray[i] === removeItem[j]) {
         newArray.splice([i], 1);
       }
@@ -29,3 +13,4 @@ function without(sourceArray, removeItem) {
   }
   return newArray;
 }
+module.exports = without;
