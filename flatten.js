b1c2e564f3fc822a20eba01312b25997d56a2b9d
amyfranses <amyfranses@gmail.com>
2022-02-19 21:13:05 +0000

    add module.exports, removed test code into separate file

--- flatten.js
+++ flatten.js
@@ -2,8 +2,6 @@
 const flatten = function (inputArray) {
   let newArray = [];
   for (let i = 0; i < inputArray.length; i++) {
-    // if array is array when iterating through main array(true)
-    // inner array flatten and concat add to new Array
     if (Array.isArray(inputArray[i])) {
       newArray = newArray.concat(flatten(inputArray[i]));
     } else {
@@ -12,4 +10,6 @@ const flatten = function (inputArray) {
   }
   return newArray;
 };
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
+
+module.exports = flatten;
+
