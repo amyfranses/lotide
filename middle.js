7a55be774038d6e452e587e0ae19aee841e84e95
amyfranses <amyfranses@gmail.com>
2022-02-19 21:31:05 +0000

    remove unnecessary comments

--- middle.js
+++ middle.js
@@ -1,19 +1,15 @@
 const assertArraysEqual = require("./assertArraysEqual");
-
+// retrieves the middle element(s) of input array
 const middle = function (array) {
   if (array.length <= 2) {
     return [];
   }
   const middleElement = array.length / 2;
-  // array of odd length return middle
   if (array.length % 2 === 1) {
     return array[Math.floor(middleElement)];
   } else {
-    // array of even elements return 2 elements
     return [array[middleElement - 1], array[middleElement]];
   }
 };
-
-console.log(middle([1, 2, 4, 5, 7, 3]));
 
 module.exports = middle;
